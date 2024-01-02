52ee16aac07d792dd931724c0ff5bb81e27cbbab
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2024-01-02 17:07:43 +0530

    changes

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "jazzmin",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -119,6 +120,9 @@ STATIC_URL = "static/"
 MEDIA_URL = "/images/"
 MEDIA_ROOT = BASE_DIR/'static'
 
+STATICFILES_DIRS = [
+    BASE_DIR/'static'
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
