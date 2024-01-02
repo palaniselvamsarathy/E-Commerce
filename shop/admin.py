6623dafc051951a8c164e26bb0a3c47299f7ca46
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2024-01-02 15:13:21 +0530

    changes

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from shop.models import *
 # Register your models here.
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display= ('name','image','description')
+
+# admin.site.register(Category,CategoryAdmin)
+admin.site.register(Category)
+admin.site.register(Products)
