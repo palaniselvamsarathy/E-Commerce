2f5c1e2469ce46c1617b796f630c0787833a1429
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2024-01-04 00:02:55 +0530

    changes

--- shop/urls.py
+++ shop/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("",views.home,name="home"),
-    path("reg/",views.register,name="register"),
+    path("register/",views.register,name="register"),
     path("collections/",views.collections,name="collections"),
     path("collections/<str:name>",views.collectionsview,name="collections"),
     path("collections/<str:cname>/<str:pname>",views.product_details,name="product_details"),
+    path("login/",views.login_page,name='login'),
+    path("logout/",views.logout_page,name='logout'),
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,17 +1,49 @@
 from django.shortcuts import render,redirect
 from shop.models import Category,Products
 from django.contrib import messages
-from django.http import JsonResponse
-
+# from django.http import JsonResponse
+from shop.forms import CustomUserForm
+from django.contrib.auth import login,authenticate,logout
 
 # Create your views here.
 def home(request):
     products = Products.objects.filter(trending=1)
     return render(request,'shop/index.html',{"products":products})
 
+def logout_page(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request,"Logged out Successfully")
+    return redirect("/")
+
+
+def login_page(request):
+    if request.user.is_authenticated:
+       return redirect("/")
+    else:
+        if request.method == 'POST':
+            name= request.POST.get('username')
+            pwd = request.POST.get('password')
+            user = authenticate(request,username=name,password=pwd)
+            if user is not None:
+                login(request,user)
+                messages.success(request,'Logged in Successfully')
+                return redirect("/")
+            else:
+                messages.error(request,'Invalid Username or Password!!!')
+                return redirect('/login')
+        return render(request,'shop/login.html')
+
 
 def register(request):
-    return render(request,'shop/register.html')
+  form=CustomUserForm()
+  if request.method=='POST':
+    form=CustomUserForm(request.POST)
+    if form.is_valid():
+      form.save()
+      messages.success(request,"Registration Success You can Login Now..!")
+      return redirect('/login')
+  return render(request,"shop/register.html",{'form':form})
 
 
 def collections(request):
